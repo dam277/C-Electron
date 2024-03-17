f48844d8713910bbe33dbf9a6afa20944f25e455
dam277 <damis.loup@gmail.com>
2024-03-17 20:38:39 +0100

    basic files

---
+++ 02-SourceCode/src/main.ts
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from "electron";
+import path from "node:path"
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile(`${__dirname}/index.html`)
+}
+
+app.whenReady().then(createWindow)
